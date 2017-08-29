d28ab1ce357026252b65a3c0825c1ea2aa374ab6
ONEPRO301\sopor <ericmedicruz@gmail.com>
2017-08-29 10:08:16 -0500

    fix

--- EjercicioOneProEjem/Controllers/AccountController.cs
+++ EjercicioOneProEjem/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using EjercicioOneProEjem.Models;
+using DAL_OneProEjemplo;
 
 namespace EjercicioOneProEjem.Controllers
 {
